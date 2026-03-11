46bbcf25c812b4ed2cd32f98c87f0c3b3f780678
Divya Dharshini <divyadharshinid77@gmail.com>
2026-03-11 20:03:18 +0530

    First commit

---
+++ task1.js
@@ -0,0 +1,17 @@
+// arrow function to add 2 num
+let a=10;
+let b=20;
+const add_num=(a,b)=>
+{
+    return a+b;
+}
+console.log(add_num(a,b));
+
+
+// anonymous function to add 2 num
+let num1=30;
+let num2=50;
+const sum=function(num1,num2){
+    return num1+num2;
+}
+console.log(sum(num1,num2));
