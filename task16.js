cc9e4abb02c68a6e1510b25a80d0c4092ef737f5
Divya Dharshini <divyadharshinid77@gmail.com>
2026-03-12 12:39:05 +0530

    if else if else(12/3/26)

---
+++ task16.js
@@ -0,0 +1,16 @@
+const mark=95;
+if(mark>=90){
+    console.log("Grade A");
+}
+else if(mark>=70){
+    console.log("Grade B");
+}
+else if(mark>=50){
+    console.log("Grade C");
+}
+else if(mark>=40){
+    console.log("Pass");
+}
+else{
+    console.log("Fail");
+}
