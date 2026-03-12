cf55da663042e12edbd72ec79430ad1ef6e62176
Divya Dharshini <divyadharshinid77@gmail.com>
2026-03-12 12:41:32 +0530

    loops(12/3/26)

---
+++ task18.js
@@ -0,0 +1,29 @@
+// for loop
+console.log("For loop");
+for(let i=0;i<=5;i++){
+        console.log("i value is: ",i);
+    }
+
+// while loop
+console.log("While loop");
+let i=1;
+while(i<=6){
+    console.log("i value is: ",i);
+    i++;
+}
+
+// do while loop
+console.log("Do while loop");
+let a=2;
+do{
+    console.log("a value is: ",a);
+    a++;
+}
+while(a<1);
+
+// for of loop
+console.log("For of loop");
+array=[1,2,3,4,5,6,7];
+for(let ele of array){
+    console.log("array ele are: "+ele);
+}
