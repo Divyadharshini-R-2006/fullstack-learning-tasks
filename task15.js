4388fd39027647ff15123e05914f43a26afd8700
Divya Dharshini <divyadharshinid77@gmail.com>
2026-03-12 12:38:03 +0530

    switch statement(12/3/26)

---
+++ task15.js
@@ -0,0 +1,17 @@
+const day="Thu";
+switch(day){
+    case "Mon":
+        console.log("Today is Monday");
+        break;
+    case "Tue":
+        console.log("Today is Tuesday");
+        break;
+    case "Wed":
+        console.log("Today is Wednesday");
+        break;
+    case "Thu":
+        console.log("Today is Thursday");
+        break;
+    default:
+        console.log("Wow what a wonderful day....!");
+}
