12f6772dc868b33c4799240a828fd5340127c1d1
Divya Dharshini <divyadharshinid77@gmail.com>
2026-03-12 12:57:28 +0530

    operators(12/3/26)

---
+++ task19.js
@@ -0,0 +1,42 @@
+const a=10;
+const b=20;
+// Arithmetic operator
+console.log("Arithmetic operator");
+console.log("Addition of 10 and 20 is: ",(a+b));
+console.log("Subtraction of 10 and 20 is: ",(a-b));
+console.log("Multiplication oof 10 and 20 is: ",(a*b));
+console.log("Division of 10 and 20 is: ",(a/b));
+console.log("Modulo division of 10 and 20 is: ",(a%b));
+
+// Comparison operator
+console.log("Comparison operator");
+console.log("b value is greater than a: ",(b>a));
+console.log("a value is smaller than b: ",(a<b));
+console.log("a value and b value are same: ",(a==b));
+console.log("a value is greater than or equal to: ",(a>=b));
+console.log("b value is smaller than or equal to: ",(a<=b));
+
+// logical operator
+console.log("Logical operator");
+console.log("Logical AND evaluates: ",(a==b&&a<b));
+console.log("Logical OR evaluates: ",(a<b||b>a));
+console.log("Logical NOT evaluates: ",(!a==b));
+
+// Assignment operator
+console.log("Assignment operator");
+let x=10;
+let y=20;
+console.log("c value is: ",(c=6));
+console.log("a value is add by 1: ",(x+=1));
+console.log("b value is sub by 2: ",(y-=1));
+console.log("a value is multi by 3: ",(x*=3));
+console.log("b value is divide by 2: ",(y%=2));
+
+// Typeof operator
+console.log("Typeof operator");
+num1=2;
+str1="divya";
+arr1=[1,2,3,4,5];
+console.log("Type of operator a is: ",typeof(num1));
+console.log("Type of operator str1 is: ",typeof(str1));
+console.log("Type of operator arr1 is: ",typeof(arr1));
